ad671e2d090fb12a5b689cb1f6da5df4023acfff
Yusuke_Kurihara <kurihara@infocity.co.jp>
2022-11-18 15:24:46 +0900

    render対応

--- routes/authentication-ensurer.js
+++ routes/authentication-ensurer.js
@@ -4,7 +4,8 @@ function ensure(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/login');
+  //res.redirect('/login');
+  res.redirect('/login?from=' + req.originalUrl);
 }
 
 module.exports = ensure;
